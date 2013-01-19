e34ec9207197b7865a925c4d29cbca8408f55ae1
facascante <cfajr@rocketmail.com>
2013-01-19 12:41:07 +0800

    adding mongodb template

---
+++ config/sprout.js
@@ -0,0 +1,4 @@
+module.exports = {		
+	MONGO_TRANS_URL : "mongodb://localhost/trans",
+	MONGO_AUTH_URL : "mongodb://localhost/auth"
+};
--- model/transaction.mongo.js
+++ model/transaction.mongo.js
@@ -1,2 +1,60 @@
 var mongoq = require('mongoq');
-var db = mongoq(cfg.GetDB() +cfg.GetDBSchema());
+var cfg = require('../config/sprout.js');
+
+var trans = mongoq(cfg.MONGO_TRANS_URL);
+var UNSET = 'undefined';
+
+module.exports = {	
+	findAll : function(callback, clt, cnd, fmt, srt, skp, lmt){				
+		if (typeof cnd == UNSET){ cnd = {};};
+		if (typeof fmt == UNSET){ fmt = {};};
+		if (typeof srt == UNSET){ srt = {};};
+		if (typeof lmt == UNSET){ lmt = 100;};
+		if (typeof skp == UNSET){ skip = 0;};
+			
+		trans.collection(clt)
+		.find(cnd,fmt)
+		.sort(srt)
+		.skip(skp || 0)
+		.limit(lmt)
+		.toArray()
+		.done(function(data){	
+			callback(null,data);
+		}).fail( function( err ){ 
+			callback(err);	
+		});
+	},
+	findOne : function(callback, clt, cnd, fmt){
+		if (typeof cnd == UNSET){ cnd = {};};
+		if (typeof fmt == UNSET){ fmt = {};};
+			
+		trans.collection(clt)
+		.findOne(cnd,fmt)
+		.done(function(data){	
+			callback(null,data);
+		}).fail( function( err ){ 
+			callback(err);	
+		});			
+		},
+	updateOne : function(callback, clt, cnt, cnd){			
+			
+		trans.collection(clt)
+	    .update(cnd, cnt, {safe: true})
+	    .done(function(data) {   
+	    	callback(null,data);
+	    })
+		.fail( function( err ) { 
+			callback(err);	
+		});
+	},
+	removeOne : function(callback,clt,cnd){
+		trans.collection(clt)
+	    .remove(cnd, {safe: true})
+	    .done(function(data) {   
+	    	callback(null,data);
+	    })
+		.fail( function( err ) { 
+			callback(err);	
+		});
+	}
+};
