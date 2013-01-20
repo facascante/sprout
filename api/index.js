b5f322b3caeefd65b7aad06e6dcd0e9abeade55a
facascante <cfajr@rocketmail.com>
2013-01-20 08:09:40 +0800

    Implement vhost

---
+++ api/index.js
@@ -0,0 +1,39 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , page = require('./routes/page')
+  , api = require('./routes/api')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/:collection',api.findAll);
+app.get('/:collection/:id',api.findOne);
+app.post('/:collection',api.insertOne);
+app.put('/:collection/:id',api.updateOne);
+app.del('/:collection/:id',api.removeOne);
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
