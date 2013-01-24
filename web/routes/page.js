9f7fcadd6a5b11e1959739ee3d0cc9fba0b159d9
facascante <cfajr@rocketmail.com>
2013-01-24 10:23:19 +0800

    Grid Setting From Database

--- web/routes/page.js
+++ web/routes/page.js
@@ -1,3 +1,4 @@
+var trans = require('../model/trans.mongo.js');
 
 module.exports = {	
 
@@ -6,42 +7,22 @@ module.exports = {
 	},
 	module : function(req,res){
 		
-		var go ={ 
-			url:'/api/grid/menu',
-			datatype: "json", 
-			mtype:'POST',
-			sortable:true,
-			sortname: 'id',
-			rowNum:100,
-			recordpos: 'left', 
-			height: 450, 
-			width: 'auto',
-			jsonReader: {
-			  repeatitems: false,
-			  root: "rows"
-			},
-			colNames:['ID','Menu Name', 'Is Expanded?', 'Level','Is Leaf?','Is Loaded?','Index','Parent','Url'], 
-			colModel:[ 
-			 {name:'id',index:'id', width:120, hidden:true, editable: false}, 
-			 {name:'menu',index:'menu', width:150, editable: true, edittype:'text',editoptions:{dataInit:"_wac"}},
-			 {name:'expanded',index:'expanded', width:80, editable: true, edittype:'select',editoptions:{value:"true:true;false:false"}},
-			 {name:'level',index:'level', width:80, editable: true},
-			 {name:'isLeaf',index:'isLeaf', width:80, editable: true, edittype:'select',editoptions:{value:"true:true;false:false"}},
-			 {name:'loaded',index:'loaded', width:80, editable: true, edittype:'select',editoptions:{value:"true:true;false:false"}},
-			 {name:'index',index:'index', width:80, editable: true},
-			 {name:'parent',index:'parent', width:80, editable: true},
-			 {name:'url',index:'url', width:80, editable: true}
-			 ], 
-			multiselect: true,  
-			gridview: true, 
-			shrinkToFit:false,
-			viewrecords:true,
-			emptyrecords:"No records yet",
-			rownumbers: true,
-			caption: "Menu Records",
-			pager: '#grid_pager',
-		};
 		
-		res.render('module',{module:'menu',go:JSON.stringify(go)});
+		var clt = 'grid';
+		var cnd = {grid:req.params.module};
+		trans.findOne(function(err,data){
+			if(err){ res.json(err,400); }
+			else{ 
+				if(data){
+					delete data._id;
+					delete data.grid;
+				}
+				else{
+					data = {};
+				}
+				data = JSON.stringify(data);
+				res.render('module',{module:req.params.module,go:data});
+			}	
+		}, clt, cnd);
 	}
 };
