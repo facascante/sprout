4bad5853acec8f3849317d659e5cf7058f0cb51b
facascante <cfajr@rocketmail.com>
2013-04-09 16:12:31 +0800

    latest code

--- config/sprout.js
+++
@@ -1,4 +0,0 @@
-module.exports = {		
-	MONGO_TRANS_URL : "mongodb://localhost/trans",
-	MONGO_AUTH_URL : "mongodb://localhost/auth"
-};
--- sprout.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-
-express()
-.use(express.vhost('sprout.ph', require('./web')))
-.listen(80,function(){
-console.log("=============================SPROUT READY===============================");
-});
